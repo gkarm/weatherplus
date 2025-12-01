9f3254ff5943ba8a86b034e7c5a65aaf28c95388
gkarm <jlapalo@yahoo.com>
2025-12-01 17:09:07 +0100

    navbarcss

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,38 +1,114 @@
-import {Router, Route, Link,} from "react-router-dom";
-// import  { useContext } from 'react';
-// import {Link, Route, Routes} from 'react-router-dom';
-// import { AppContext } from '../../context/AppContext';
-import {AppContext} from '../../Context/AppContext.jsx';
-import WeeklyWeather from "../../pages/WeekForecast.jsx";
-import HourlyForecast from "../../pages/WeatherPlusApp/HourlyForecast.jsx";
-import MapPage from "../../pages/MapPage.jsx";
-import './Navbar.css';
-import LoginForm from "../../pages/LoginForm.jsx";
-import {useContext} from "react";
 
+
+import { useState, useContext } from "react";
+import { Link } from "react-router-dom";
+import { AppContext } from "../../Context/AppContext.jsx";
+import "./Navbar.css";
 
 const Navbar = () => {
     const { user, logout } = useContext(AppContext);
+    const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <div className='navbar'>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="weekForecast">7 Days Forecast</Link></li>
-                <li><Link to="/hourly">Hourly Forecast</Link></li>
-                <li><Link to="/map">Map</Link></li>
-                {user ? (
-                    <>
-                        <li><span>Welcome, {user.name}</span></li>
-                        <li><button onClick={logout}>Logout</button></li>
-                    </>
-                ) : (
-                    <li><Link to="/login">Register/Sign In</Link></li>
-                )}
-            </ul>
+        <nav className="navbar">
+            <div className="nav-container">
+                <div className="nav-brand">
+                    <Link to="/">WeatherApp</Link>
+                </div>
 
-        </div>
+                <button className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
+
+                <ul className={`nav-menu ${isOpen ? "active" : ""}`}>
+                    <li>
+                        <Link to="/" onClick={() => setIsOpen(false)}>
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/weekForecast" onClick={() => setIsOpen(false)}>
+                            7 Days Forecast
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/hourly" onClick={() => setIsOpen(false)}>
+                            Hourly Forecast
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/map" onClick={() => setIsOpen(false)}>
+                            Map
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/blog" onClick={() => setIsOpen(false)}>
+                            Blog
+                        </Link>
+                    </li>
+
+                    {user ? (
+                        <>
+                            <li>
+                                <span>Welcome, {user.name}</span>
+                            </li>
+                            <li>
+                                <button onClick={logout}>Logout</button>
+                            </li>
+                        </>
+                    ) : (
+                        <li>
+                            <Link to="/login" onClick={() => setIsOpen(false)}>
+                                Register/Sign In
+                            </Link>
+                        </li>
+                    )}
+                </ul>
+            </div>
+        </nav>
     );
 };
 
 export default Navbar;
+
+
+// import {Router, Route, Link,} from "react-router-dom";
+// // import  { useContext } from 'react';
+// // import {Link, Route, Routes} from 'react-router-dom';
+// // import { AppContext } from '../../context/AppContext';
+// import {AppContext} from '../../Context/AppContext.jsx';
+// import WeeklyWeather from "../../pages/WeekForecast.jsx";
+// import HourlyForecast from "../../pages/WeatherPlusApp/HourlyForecast.jsx";
+// import MapPage from "../../pages/MapPage.jsx";
+// import './Navbar.css';
+// import LoginForm from "../../pages/LoginForm.jsx";
+// import {useContext} from "react";
+//
+//
+// const Navbar = () => {
+//     const { user, logout } = useContext(AppContext);
+//
+//     return (
+//         <div className='navbar'>
+//             <ul>
+//                 <li><Link to="/">Home</Link></li>
+//                 <li><Link to="weekForecast">7 Days Forecast</Link></li>
+//                 <li><Link to="/hourly">Hourly Forecast</Link></li>
+//                 <li><Link to="/map">Map</Link></li>
+//                 {user ? (
+//                     <>
+//                         <li><span>Welcome, {user.name}</span></li>
+//                         <li><button onClick={logout}>Logout</button></li>
+//                     </>
+//                 ) : (
+//                     <li><Link to="/login">Register/Sign In</Link></li>
+//                 )}
+//             </ul>
+//
+//         </div>
+//     );
+// };
+//
+// export default Navbar;
--- src/Components/firebase.jsx
+++ src/Components/firebase.jsx
@@ -1,18 +1,18 @@
-import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth'
-
-const firebaseConfig = {
-    apiKey: "AIzaSyBVX93gULe-SqTISGQNlhrz8TX2HYFu4Tk",
-    authDomain: "weatherplus-51c48.firebaseapp.com",
-    projectId: "weatherplus-51c48",
-    storageBucket: "weatherplus-51c48.appspot.com",
-    messagingSenderId: "962047294014",
-    appId: "1:962047294014:web:e3d070ccf0fa12a2916f6f",
-    measurementId: "G-DQ1HZNGJS4"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
-
-export {auth}
+// import { initializeApp } from "firebase/app";
+// import {getAuth} from 'firebase/auth'
+//
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBVX93gULe-SqTISGQNlhrz8TX2HYFu4Tk",
+//     authDomain: "weatherplus-51c48.firebaseapp.com",
+//     projectId: "weatherplus-51c48",
+//     storageBucket: "weatherplus-51c48.appspot.com",
+//     messagingSenderId: "962047294014",
+//     appId: "1:962047294014:web:e3d070ccf0fa12a2916f6f",
+//     measurementId: "G-DQ1HZNGJS4"
+// };
+//
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app)
+//
+// export {auth}
