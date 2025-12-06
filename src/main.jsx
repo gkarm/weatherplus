a161d75df1778cc234c842316287e194c6d90c8e
gkarm <jlapalo@yahoo.com>
2025-12-06 16:50:38 +0100

    update of main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,60 +1,90 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 import WeatherPlusApp from "./pages/WeatherPlusApp/WeatherPlusApp.jsx";
-import './pages/WeatherPlusApp/WeatherPlusApp.css';
-import './Components/style.css';
+import "./pages/WeatherPlusApp/WeatherPlusApp.css";
+import "./Components/style.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import SignUpForm from './pages/SignUpForm.jsx';
+import SignUpForm from "./pages/SignUpForm.jsx";
 import LoginForm from "./pages/LoginForm.jsx";
-import Layout from './Components/Layout.jsx';
-// import Layout from './Components/Layout/Layout.jsx';
-// import { AppProvider } from './context/AppContext';
-import { AppProvider} from './Context/AppContext.jsx';
+import Layout from "./Components/Layout.jsx";
+import { AppProvider } from "./Context/AppContext.jsx";
 import WeekForecast from "./pages/WeekForecast.jsx";
-import MapPage from './pages/MapPage.jsx';
+import MapPage from "./pages/MapPage.jsx";
 import HourlyForecast from "./pages/WeatherPlusApp/HourlyForecast.jsx";
+import Blog from "./pages/Blog.jsx";
 
 const router = createBrowserRouter([
     {
-        path: '/',
-        element: <Layout><WeatherPlusApp /></Layout>,
+        path: "/",
+        element: (
+            <Layout>
+                <WeatherPlusApp />
+            </Layout>
+        ),
     },
     {
-        path: 'signup',
-        element: <Layout><SignUpForm /></Layout>,
+        path: "signup",
+        element: (
+            <Layout>
+                <SignUpForm />
+            </Layout>
+        ),
     },
     {
-        path: 'weatherPlusApp',
-        element: <Layout><WeatherPlusApp /></Layout>,
+        path: "weatherPlusApp",
+        element: (
+            <Layout>
+                <WeatherPlusApp />
+            </Layout>
+        ),
     },
     {
-        path: 'login',
-        element: <Layout><LoginForm /></Layout>,
+        path: "login",
+        element: (
+            <Layout>
+                <LoginForm />
+            </Layout>
+        ),
     },
     {
-      path: 'weekForecast',
-      element: <Layout><WeekForecast /></Layout>
+        path: "weekForecast",
+        element: (
+            <Layout>
+                <WeekForecast />
+            </Layout>
+        ),
     },
     {
-      path: 'map',
-      element: <Layout><MapPage /></Layout>
+        path: "map",
+        element: (
+            <Layout>
+                <MapPage />
+            </Layout>
+        ),
     },
     {
-        path:'hourly',
-        element: <Layout><HourlyForecast /></Layout>
+        path: "hourly",
+        element: (
+            <Layout>
+                <HourlyForecast />
+            </Layout>
+        ),
     },
-
-
+    {
+        path: "blog",
+        element: (
+            <Layout>
+                <Blog />
+            </Layout>
+        ),
+    },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-
         <AppProvider>
             <RouterProvider router={router} />
         </AppProvider>
-
-    </React.StrictMode>,
+    </React.StrictMode>
 );
